b90ea0d462263474d98eeca1529e41b5c2225037
Matheus dos Santos Soares <maverick17xd@gmail.com>
2025-05-14 15:05:05 -0300

    :tada: init: initial portfolio setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import NavBar from "./sections/NavBar";
+
+function App() {
+  return (
+    <main className="max-w-7xl mx-auto">
+      <NavBar />
+    </main>
+  );
+}
+
+export default App;
