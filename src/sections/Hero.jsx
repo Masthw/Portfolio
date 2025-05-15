b7ab7f56f5d922aca792b1c49eb82a1fd81bfa39
Matheus dos Santos Soares <maverick17xd@gmail.com>
2025-05-15 13:48:42 -0300

    :sparkles: feat: create HackerRoom Component and implements on Hero Section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,20 @@
 import { PerspectiveCamera } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { HackerRoom } from "../components/HackerRoom";
+import { Suspense } from "react";
+import CanvasLoader from "../components/CanvasLoader";
+
+import { useMediaQuery } from "react-responsive";
+import { calculateSizes } from "../constants";
 
 const Hero = () => {
+  const isSmall = useMediaQuery({ query: "(max-width: 440px)" });
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+  const isTablet = useMediaQuery({
+    query: "(min-width: 768px)(max-width: 1024px)",
+  });
+
+  const sizes = calculateSizes(isSmall, isMobile, isTablet);
   return (
     <section className="min-h-screen w-full flex flex-col relative">
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
@@ -11,8 +24,21 @@ const Hero = () => {
         <p className="hero-tag text-gray-gradient">Software Developer</p>
       </div>
       <div className="w-full h-full absolute inset-0">
+       {/*  <Leva /> */}
         <Canvas className="w-full h-full">
-          <PerspectiveCamera makeDefault position={[0, 0, 30]}/>
+          <Suspense fallback={<CanvasLoader />}>
+            <PerspectiveCamera makeDefault position={[0, 0, 20]} />
+            <HackerRoom
+              scale={sizes.deskScale}
+              position={sizes.deskPosition}
+              rotation={[0, -Math.PI, 0]}
+            />
+            <group>
+              
+            </group>
+            <ambientLight intensity={1} />
+            <directionalLight position={[10, 10, 10]} intensity={0.5} />
+          </Suspense>
         </Canvas>
       </div>
     </section>
