0b8fb5cf978c0c887d3239aaa31c610306d6eed3
Matheus dos Santos Soares <maverick17xd@gmail.com>
2025-05-20 10:46:19 -0300

    :art: style: add heade text to About section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -13,7 +13,8 @@ const About = () => {
   };
   return (
     <section className="c-space my-20">
-      <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5">
+       <p className="head-text text-gray-gradient">About</p>
+      <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 mt-12">
         <div className="col-span-1 xl:row-span-3">
           <div className="grid-container">
             <img
