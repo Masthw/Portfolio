74776eb811eff4d5baab6b55d61f577f410ef50b
Matheus dos Santos Soares <maverick17xd@gmail.com>
2025-05-28 09:32:17 -0300

    :package: chore: add profile picture to about

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -13,20 +13,23 @@ const About = () => {
   };
   return (
     <section className="c-space my-20" id="about">
-       <p className="head-text text-gray-gradient">About</p>
+      <p className="head-text text-gray-gradient">About</p>
       <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 mt-12">
         <div className="col-span-1 xl:row-span-3">
           <div className="grid-container">
             <img
-              src="assets/grid1.png"
+              src="assets/me3.png"
               alt="grid-1"
-              className="w-full sm:h-[276px] h-fit object contain"
+              className="w-full h-auto object-contain"
             />
             <div>
               <p className="grid-headtext">Hi, I'm Matheus</p>
               <p className="grid-subtext">
-                With 2 years of experience, I have honed my skills in frontend
-                and backend development, with a focus on animated 3D websistes.
+                I'm a frontend developer with 2 years of experience. While my
+                primary focus is building modern, responsive web applications,
+                I'm also passionate about creative technologies like 3D web
+                development, which I'm continuously exploring and incorporating
+                into my work.
               </p>
             </div>
           </div>
@@ -41,8 +44,9 @@ const About = () => {
             <div>
               <p className="grid-headtext">Tech Stack</p>
               <p className="grid-subtext">
-                I specialize in JavaScript/Typescript with a focus on React and
-                Next.js ecosystems.{" "}
+                I specialize in frontend development using JavaScript and
+                TypeScript, with a focus on React, React Native, Vue.js and Next.js. I
+                also build cross-platform apps with Flutter.
               </p>
             </div>
           </div>
@@ -80,11 +84,11 @@ const About = () => {
               className="w-full sm:h-[266px] h-fit object-contain"
             />
             <div>
-              <p className="grid-headtext">My Passion for Coding</p>
-              <p className="grid-subtext">
-                I love solving problems and building things through code. Coding
-                isn't just my profession - it is my passion.
-              </p>
+            <p className="grid-headtext">My Passion for Coding</p>
+<p className="grid-subtext">
+  I’m passionate about solving real-world problems and creating meaningful digital experiences. For me, coding is more than a career — it’s what I truly enjoy doing.
+</p>
+
             </div>
           </div>
         </div>
@@ -102,7 +106,9 @@ const About = () => {
                   src={hasCopied ? "assets/tick.svg" : "assets/copy.svg"}
                   alt="copy"
                 />
-                <p className="lg:text-2xl md:text-xl font-medium text-gray-gradient text-white">matheussoaressdev@gmail.com</p>
+                <p className="lg:text-1xl md:text-xl font-medium text-gray-gradient text-white">
+                  matheussoaressdev@gmail.com
+                </p>
               </div>
             </div>
           </div>
